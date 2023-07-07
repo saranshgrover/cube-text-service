5c3f65cd5280581e9f3eac7d83a0c603b7f9727a
Saransh Grover <contact@saranshgrover.com>
2023-07-07 14:17:41 -0500

    update production WCA_ENV

--- config/index.ts
+++ config/index.ts
@@ -2,6 +2,6 @@ export const ORIGIN_URL =
 	process.env.NODE_ENV === 'production' ? '' : 'http://localhost:3000'
 
 export const WCA_URL =
-	process.env.NODE_ENV !== 'production'
+	process.env.NODE_ENV === 'production'
 		? 'https://www.worldcubeassociation.org'
 		: 'https://staging.worldcubeassociation.org'
